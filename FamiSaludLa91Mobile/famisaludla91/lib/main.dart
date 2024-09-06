29b0d7bfb2a1b1ca3173007e85b03fea709a2edc
maiyerhn <118492521+maiyerhn@users.noreply.github.com>
2024-09-06 17:56:55 -0500

    RegisterAndLogin Funcional

--- FamiSaludLa91Mobile/famisaludla91/lib/main.dart
+++ FamiSaludLa91Mobile/famisaludla91/lib/main.dart
@@ -1,40 +1,54 @@
-
 import 'package:famisaludla91/vistas/registrarse.dart';
 import 'package:famisaludla91/vistas/vistainicio.dart';
 import 'package:famisaludla91/vistas/vistainicioadm.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Inicio(),
     );
   }
 }
 
-class Inicio extends StatelessWidget {
+class Inicio extends StatefulWidget {
+  const Inicio({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _InicioState createState() => _InicioState();
+}
+
+class _InicioState extends State<Inicio> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  bool _isPasswordVisible = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Center(child: Text('Inicio')),
+        title: const Center(child: Text('Inicio')),
         backgroundColor: Colors.blue,
         foregroundColor: Colors.white,
       ),
-
       body: SingleChildScrollView(
-        padding: EdgeInsets.all(16.0),
+        padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            SizedBox(height: 30),
-            Center(
+            const SizedBox(height: 30),
+            const Center(
               child: CircleAvatar(
                 radius: 40,
                 backgroundColor: Colors.black12,
@@ -45,99 +59,159 @@ class Inicio extends StatelessWidget {
                 ),
               ),
             ),
-
-            SizedBox(height: 30),
-            Center(
+            const SizedBox(height: 30),
+            const Center(
               child: Column(
                 children: [
                   Text(
-                    'Iniciar Sesion',
+                    'Iniciar Sesión',
                     style: TextStyle(
                       fontSize: 24,
                       fontWeight: FontWeight.bold,
                     ),
                   ),
-                  
                   SizedBox(height: 8),
                   Text(
                     '¡Hola! Me alegro de volver a verte..',
-                    style: TextStyle(color: Colors.grey), 
+                    style: TextStyle(color: Colors.grey),
                     textAlign: TextAlign.start,
                   ),
                 ],
               ),
             ),
-            SizedBox(height: 30),
-            _buildTextField(label: 'Usuario (E-mail)', hintText: 'example@email.com'),
-            _buildTextField(label: 'Contraseña', hintText: 'Ingrese Contraseña', obscureText: true),
-            SizedBox(height: 20),
+            const SizedBox(height: 30),
+            _buildTextField(
+              label: 'Usuario (E-mail)',
+              hintText: 'example@email.com',
+              controller: emailController,
+            ),
+            _buildTextField(
+              label: 'Contraseña',
+              hintText: 'Ingrese Contraseña',
+              obscureText: !_isPasswordVisible,
+              controller: passwordController,
+              suffixIcon: IconButton(
+                icon: Icon(
+                  _isPasswordVisible ? Icons.visibility : Icons.visibility_off,
+                ),
+                onPressed: () {
+                  setState(() {
+                    _isPasswordVisible = !_isPasswordVisible;
+                  });
+                },
+              ),
+            ),
+            const SizedBox(height: 20),
             Row(
               children: [
                 Expanded(
                   child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage()));
-                    },
+                    onPressed: () => _login(context),
                     style: ElevatedButton.styleFrom(
                       backgroundColor: Colors.green,
-                      foregroundColor: Colors.white
+                      foregroundColor: Colors.white,
                     ),
-                    child: Text('Ingresar'),
+                    child: const Text('Ingresar'),
                   ),
                 ),
-                SizedBox(width: 10),
+                const SizedBox(width: 10),
                 Expanded(
-                  child: ElevatedButton(  
+                  child: ElevatedButton(
                     onPressed: () {
-                      Navigator.push(context, MaterialPageRoute(builder: (context) => RegistroPage()));
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => const RegistroPage()),
+                      );
                     },
                     style: ElevatedButton.styleFrom(
                       backgroundColor: Colors.blue,
-                      foregroundColor: Colors.white
+                      foregroundColor: Colors.white,
                     ),
-                    child: Text('Registrarse'),
+                    child: const Text('Registrarse'),
                   ),
                 ),
               ],
             ),
-            /*ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.green,
-              ),
-              child: Text('Ingresar'),
-            ),*/
-            SizedBox(height: 10),
+            const SizedBox(height: 10),
             GestureDetector(
               onTap: () {
-                // ingresar a la vista de administrador
                 Navigator.push(context, MaterialPageRoute(builder: (context) => Inicioad()));
               },
-              child: Text(
+              child: const Text(
                 '¿Olvidaste tu contraseña?',
                 style: TextStyle(color: Colors.blue),
                 textAlign: TextAlign.center,
               ),
             ),
-            SizedBox(height: 20),
+            const SizedBox(height: 20),
           ],
         ),
       ),
     );
   }
 
-  Widget _buildTextField({required String label, required String hintText, bool obscureText = false}) {
+  Widget _buildTextField({
+    required String label,
+    required String hintText,
+    bool obscureText = false,
+    required TextEditingController controller,
+    Widget? suffixIcon,
+  }) {
     return Padding(
       padding: const EdgeInsets.only(bottom: 16.0),
       child: TextField(
+        controller: controller,
         obscureText: obscureText,
         decoration: InputDecoration(
           labelText: label,
           hintText: hintText,
-          suffixIcon: obscureText ? Icon(Icons.visibility) : null,
-          border: OutlineInputBorder(),
+          suffixIcon: suffixIcon,
+          border: const OutlineInputBorder(),
         ),
       ),
     );
+  }
+
+  Future<void> _login(BuildContext context) async {
+    final String email = emailController.text;
+    final String password = passwordController.text;
+
+    try {
+      final url = Uri.parse('https://feaf-45-238-146-4.ngrok-free.app/login');
+      final response = await http.post(
+        url,
+        body: jsonEncode({'email': email, 'password': password}),
+        headers: {'Content-Type': 'application/json'},
+      );
+
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body);
+        final String role = data['user']['role'];
+
+        if (role == 'ADMINISTRADOR') {
+          // ignore: use_build_context_synchronously
+          Navigator.push(context, MaterialPageRoute(builder: (context) => Inicioad()));
+        } else if (role == 'CLIENTE') {
+          // ignore: use_build_context_synchronously
+          Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage()));
+        } else {
+          // ignore: use_build_context_synchronously
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('Rol desconocido: $role')),
+          );
+        }
+      } else {
+        final error = jsonDecode(response.body)['errors'];
+        // ignore: use_build_context_synchronously
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('$error')),
+        );
+      }
+    } catch (e) {
+      // ignore: use_build_context_synchronously
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Error de conexión: $e')),
+      );
+    }
   }
 }
